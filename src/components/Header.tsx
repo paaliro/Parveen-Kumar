b311506065b45a6892b3ab732dfc1eaaab2da725
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-04-07 23:03:09 +0530

    code updated & setup completed to code

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <p className="text-4xl font-semibold text-center py-10">Hello, this side Parveen.</p>
+        </div>
+    )
+}
+
+export default Header
