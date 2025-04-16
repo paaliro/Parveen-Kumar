2df4042221f0fb1ef60bb70250992727364a8e9f
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-04-16 23:37:23 +0530

    today's work

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,32 +6,24 @@ const words = [
 
 function Hero() {
     return (
-        <section id="Hero" className="">
-            {/* Left Section */}
-            <header>
-                <div className="text-4xl text-center pt-10">
-                    <h1 className="flex gap-4">Shaping
-                        <span className="slide">
-                            <span className="wrapper">
-                                {words.map((word) => (
-                                    <span key={word.text} className="flex items-center justify-center md:gap-3 gap-1 pb-2">
-                                        <img
-                                            src={word.imgPath}
-                                            alt={word.text}
-                                            className="md:size-10 size-7 p-1 rounded-full bg-white-50" />
-                                    </span>
-                                ))}
-                            </span>
-                        </span>
-                    </h1>
-                    <h1>into Real Projects</h1>
-                    <h1>that Deliver Results</h1>
-                </div>
-            </header>
+        <section id="Hero" className="relative overflow-hidden">
+            <div className="absolute top-0 left-0 z-10">
+                <img src="/images/bg.png" alt="background" />
+            </div>
 
-
-            {/* Right Section */}
-
+            <div className="hero-layout">
+                {/* Left : Hero Content */}
+                <header className="flex flex-col justify-center md:w-full w-screen">
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text text-4xl">
+                            <h1>Shapping</h1>
+                            <h1>into Real Projects</h1>
+                            <h1>that Deliver Results</h1>
+                        </div>
+                    </div>
+                </header>
+                {/* Right : 3d Model */}
+            </div>
         </section>
     )
 }
