f52eb955477a9019e157ddd94751091211ed8990
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-04-23 00:36:12 +0530

    ignore

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,20 +8,20 @@ const words = [
 function Hero() {
     return (
         <section id="Hero" className="relative overflow-hidden">
-            <div className="absolute top-0 left-0 z-10 w-screen">
+            <div className="absolute top-0 left-0 z-10 w-screen sm:w-full">
                 <img src="/images/bg.png" alt="background" />
             </div>
 
             <div className="hero-layout">
                 {/* Left : Hero Content */}
                 <header className="flex flex-col justify-center sm:w-full w-screen">
-                        <div className="flex flex-col hero-text text-2xl sm:text-4xl text-start sm:text-center ml-4 mt-6 sm:mt-10">
+                        <div className="flex flex-col hero-text text-2xl sm:text-4xl text-start sm:text-center ml-4 mt-20 sm:mt-15">
                             <div className="flex sm:justify-center items-center">
                                 <h1>Shapping</h1>
                                 <div className="slide h-[46px] overflow-hidden">
                                     <div className="wrapper flex flex-col animate-slide">
                                         {words.map((word, index) => (
-                                            <div key={index} className="flex items-center gap-1 p-2 h-[60px]">
+                                            <div key={index} className="flex items-center gap-1 pl-2 pb-2 h-[60px]">
                                                 <img src={word.imgPath} alt={word.text} className="w-7 sm:w-8 h-7 sm:h-8 bg-white rounded-full p-1" />
                                                 <span>{word.text}</span>
                                             </div>
