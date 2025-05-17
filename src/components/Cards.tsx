f44583f2fcb825616c9d53bb8caaaa465f205aef
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-05-17 09:09:14 +0530

    final update for cards comp.

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,12 +5,12 @@ const Cards = ({ className }: { className?: string }) => {
     return (
         <div className={`${className} grid w-[90%] sm:w-[97%] gap-2 mx-5 sm:flex mt-90 sm:mt-5 mb-20`}>
             {cardsContent.map((data, index) => (
-                <div key={index} className="flex-1 bg-zinc-800 rounded-2xl px-4 py-7">
-                    <div className="flex gap-1 text-3xl font-bold">
+                <div key={index} className="flex-1 bg-zinc-800 rounded-2xl px-4 py-5 sm:py-7">
+                    <div className="flex gap-1 text-2xl sm:text-3xl font-bold">
                         <p>{data.value}</p>
                         <p>{data.symbol}</p>
                     </div>
-                    <p>{data.label}</p>
+                    <p className="text-sm sm:text-base">{data.label}</p>
                 </div>
             ))}
         </div>
