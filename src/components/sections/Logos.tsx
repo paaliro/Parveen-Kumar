af3264602ee1c4b42e58c3847326ec7c0a5fc8f1
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-05-18 01:54:54 +0530

    logos chnages done.

--- src/components/sections/Logos.tsx
+++ src/components/sections/Logos.tsx
@@ -2,11 +2,11 @@ import { logoIconsList } from "../../assets/helper";
 
 const Logos = () => {
     return (
-        <div className="overflow-hidden w-full py-15">
+        <div className="overflow-hidden w-full py-15 sm:py-40">
             <div className="flex marquee whitespace-nowrap">
                 {[...logoIconsList, ...logoIconsList].map((logo, index) => (
                     <div key={index} className="flex-none px-6 sm:px-10">
-                        <img src={logo.imgPath} alt={`Logo ${index + 1}`} className="h-8 sm:h-12 w-auto" />
+                        <img src={logo.imgPath} alt={`Logo ${index + 1}`} className="h-6 sm:h-12 w-auto" />
                     </div>
                 ))}
             </div>
