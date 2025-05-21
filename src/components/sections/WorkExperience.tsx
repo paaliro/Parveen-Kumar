904431eda88b9fe851be741f780647bffa42372e
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-05-22 00:05:41 +0530

    updated code.

--- src/components/sections/WorkExperience.tsx
+++ src/components/sections/WorkExperience.tsx
@@ -7,19 +7,15 @@ const WorkExperience = () => {
             <Tag icon="💼" text="My Career Overview" />
             <p className="text-center text-2xl sm:text-4xl font-semibold mt-1">Professional Work Experience</p>
 
-
-
-
             <div className="my-20 flex flex-col gap-2.5">
                 <div className="flex mt-4">
                     {Array.from({ length: 5 }).map((_, index) => (
-                        <ImageTag key={index} src="/images/sections/workExperience/star.svg" alt="star" width="22" height="20"/>
+                        <ImageTag key={index} src="/images/sections/workExperience/star.svg" alt="star" width="22" height="20" />
                     ))}
                 </div>
                 <p>Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.</p>
                 <ImageTag src="/images/sections/workExperience/hostinger.png" alt="company logo" width="" height="" />
             </div>
-
 
         </section>
     )
