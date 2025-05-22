a5e27a84f7bd878281c5267ecd583ae7dd58ecf8
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-05-22 23:39:15 +0530

    start working.

--- src/components/sections/WorkExperience.tsx
+++ src/components/sections/WorkExperience.tsx
@@ -3,7 +3,7 @@ import Tag from "../Extra/Tag";
 
 const WorkExperience = () => {
     return (
-        <section className="bg-[#141416] mx-4 sm:mx-9 rounded-xl mt-10 pt-3">
+        <section className="bg-[#202123] mx-4 sm:mx-9 rounded-xl mt-10 pt-3">
             <Tag icon="💼" text="My Career Overview" />
             <p className="text-center text-2xl sm:text-4xl font-semibold mt-1">Professional Work Experience</p>
 
