4b0651f4ff815102d09cc7e5f57e1d9acfb8c5db
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-05-31 15:32:55 +0530

    working.

--- src/assets/helper.ts
+++ src/assets/helper.ts
@@ -110,6 +110,56 @@ export const workContent2 = [
   },
 ];
 
+export const peopleTalk = [
+  {
+    starPath: "/images/sections/workExperience/star.svg",
+    text: "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+    profile: "/images/sections/workExperience/man.png",
+    name: "Esther Howard",
+    userName: "@estherhoward",
+  },
+
+  {
+    starPath: "/images/sections/workExperience/star.svg",
+    text: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    profile: "/images/sections/workExperience/man.png",
+    name: "Guy Hawkins",
+    userName: "@guyhawlkins",
+  },
+
+  {
+    starPath: "/images/sections/workExperience/star.svg",
+    text: "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    profile: "/images/sections/workExperience/man.png",
+    name: "Wade Warren",
+    userName: "@wadewarren",
+  },
+
+  {
+    starPath: "/images/sections/workExperience/star.svg",
+    text: "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+    profile: "/images/sections/workExperience/man.png",
+    name: "Floyd Miles",
+    userName: "@floydmiles",
+  },
+
+  {
+    starPath: "/images/sections/workExperience/star.svg",
+    text: "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    profile: "/images/sections/workExperience/man.png",
+    name: "Marvin McKinney",
+    userName: "@marvinmackinney",
+  },
+
+  {
+    starPath: "/images/sections/workExperience/star.svg",
+    text: "Adrian was great to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.",
+    profile: "/images/sections/workExperience/man.png",
+    name: "Albert Flores",
+    userName: "@albertflores",
+  },
+];
+
 // const workExperience = {
 //   box1: {
 //     left: {
@@ -162,5 +212,3 @@ export const workContent2 = [
 //     },
 //   },
 // };
-
-
--- src/components/Extra/CustomerFeedback.tsx
+++ src/components/Extra/CustomerFeedback.tsx
@@ -1,8 +1,35 @@
+import { peopleTalk } from "../../assets/helper";
+import ImageTag from "./ImageTag";
+import Tag from "./Tag";
+
 const CustomerFeedback = () => {
     return (
-        <div>
-            hello
-        </div>
+        <section className=" w-95vw mt-15">
+            <Tag icon="⭐" text="Customer feedback highlights" />
+            <h1 className="text-center font-semibold text-2xl sm:text-4xl w-90vw mb-10">What People Say About Me?</h1>
+
+            <div className="sm:grid grid-cols-3 items-baseline sm:mx-14 h-fit border-1 border-amber-50 pb-3">
+                {peopleTalk.map((data, index) => (
+                    <div key={index} className="sm:w-[95%] sm:h-fit bg-[#141416] mx-6 sm:mx-auto mt-4 rounded-lg pb-1 sm:pb-0 pt-1 sm:pt-0 border-1 border-amber-200">
+                        <div className="flex my-5 mx-5">
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <ImageTag key={index} src={data.starPath} alt="star" width="22" height="20" />
+                            ))}
+                        </div>
+                        <p className="mx-5 text-balance text-[#D9ECFF]">
+                            {data.text}
+                        </p>
+                        <div className="mt-6 flex gap-3 mx-5 my-5 items-center">
+                            <ImageTag alt="person-1" src={data.profile} width="38" height="38" />
+                            <div>
+                                <p className="font-semibold">{data.name}</p>
+                                <p className="font-light text-xs text-[#D9ECFF]">{data.userName}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
 
