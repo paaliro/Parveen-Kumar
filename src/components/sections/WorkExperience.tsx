7e69bb59d8dcac583ca643f3b29464850cd39103
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-02 22:47:13 +0530

    seeing some code.

--- src/components/sections/WorkExperience.tsx
+++ src/components/sections/WorkExperience.tsx
@@ -8,7 +8,7 @@ const WorkExperience = () => {
         <section className="bg-[#202123] mx-4 sm:mx-9 rounded-xl mt-10 pt-3">
             <Tag icon="💼" text="My Career Overview" /> 
             <p className="text-center text-2xl sm:text-4xl font-semibold mt-2 sm:mt-7 mb-10">Professional Work Experience</p>
-            <CareerOverview /> 
+            <CareerOverview />
             {/* <WhatIBring /> */}  
             <CustomerFeedback />
         </section> 
