677fcd43c016f000e37eb405a02f8697566743ad
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-07 22:18:41 +0530

    little changes

--- src/components/Extra/Cards.tsx
+++ src/components/Extra/Cards.tsx
@@ -3,7 +3,7 @@ import CountUp from 'react-countup';
 
 const Cards = ({ className }: { className?: string }) => {
     return (
-        <div className={`${className} grid w-[90%] sm:w-[96%] gap-2 mx-7 sm:mx-8 sm:flex mt-[90px] sm:mt-5`}>
+        <div className={`${className} grid w-[90%] sm:w-[96%] gap-2 mx-7 sm:mx-8 sm:flex mt-90 sm:mt-5`}>
             {cardsContent?.map((data, index) => (
                 <div key={index} className="flex-1 bg-[#202123] rounded-2xl px-4 py-5 sm:py-7">
                     <div className="flex gap-0.5 text-2xl sm:text-3xl font-bold text-white items-baseline">
