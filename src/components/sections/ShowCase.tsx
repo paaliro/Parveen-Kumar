8b8b20a672708b80f9a8976aa31f4d3fa3e66fbc
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-12 20:56:25 +0530

    showcase fixed

--- src/components/sections/ShowCase.tsx
+++ src/components/sections/ShowCase.tsx
@@ -48,7 +48,7 @@ const ShowCase = () => {
     return (
         <section ref={sectionRef} className="sm:flex justify-center mx-8 mt-8 vw-100 bg-[#202123] rounded-2xl py-4 md:py-8">
             <div ref={image1Ref} className="sm:flex flex-col justify-center mb-4 sm:mb-0 sm:w-7/12">
-                <ImageTag src="/images/sections/showCase/img-1.png" alt="showCase-img-1" className="flex justify-center w-100 md:w-full mx-auto" />
+                <ImageTag src="/images/sections/showCase/img-1.png" alt="showCase-img-1" className="flex justify-center w-full mx-auto" />
                 <div className="px-2 mt-1 leading-7 sm:leading-normal text-center">
                     <h1 className="text-[1.33rem] md:text-3xl font-semibold">On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde</h1>
                     <p className="text-xs md:text-base text-[#D9ECFF]">An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.</p>
@@ -56,11 +56,11 @@ const ShowCase = () => {
             </div>
             <div className="flex flex-col gap-6">
                 <div ref={image2Ref} className="mt-5">
-                    <ImageTag src="/images/sections/showCase/img-2.png" alt="showCase-img-2" className="w-100 md:w-full mx-auto" />
+                    <ImageTag src="/images/sections/showCase/img-2.png" alt="showCase-img-2" className="w-full mx-auto" />
                     <h1 className="font-semibold text-lg sm:text-xl mt-1 ml-2 text-center">The Library Management Platform</h1>
                 </div>
                 <div ref={image3Ref} >
-                    <ImageTag src="/images/sections/showCase/img-3.png" alt="showCase-img-3" className="w-100 md:w-full mx-auto" />
+                    <ImageTag src="/images/sections/showCase/img-3.png" alt="showCase-img-3" className="w-full mx-auto" />
                     <h1 className="font-semibold text-lg sm:text-xl mt-1 ml-2 text-center">YC Directory - A Startup Showcase App</h1>
                 </div>
             </div>
