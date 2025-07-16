d6ea9b30a790e29dc9934b32fc89e401fef080e1
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-16 22:30:13 +0530

    CareerOverview.tsx working.

--- src/components/sections/WorkExperience.tsx
+++ src/components/sections/WorkExperience.tsx
@@ -4,7 +4,7 @@ import Tag from "../Extra/Tag";
 // import WhatIBring from "../Extra/WhatIBring";
 
 const WorkExperience = () => {
-    return (
+    return ( 
         <section className="bg-[#202123] rounded-xl mt-10 pt-3 mx-2 sm:mx-9">
             <Tag icon="💼" text="My Career Overview" />  
             <p className="text-center text-2xl sm:text-4xl font-semibold mt-2 sm:mt-7 mb-10">Professional Work Experience</p>
